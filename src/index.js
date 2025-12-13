a7cff842882596034bbabbc8d65fbe4b3b9717f9
Mahika Barnwal <mahibarnwal1234@gmail.com>
2025-12-13 16:06:35 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// require('dotenv').config({path: ',/env'});
+import dotenv from "dotenv"
+
+
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First approach for connecting database where all function code write in index file directly without separating code in another file.
+/*
+import express from "express"
+
+const app = express()
+
+
+;(async () => {
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error" , (error)=>{     //listener who are listening errors here
+      console.log("Error:" , error);
+      throw error
+    })
+    
+    app.listen(process.env.PORT , () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  }
+  catch(error){
+    console.log("ERROR : " , error)
+    throw err
+  }
+})()
+  */
